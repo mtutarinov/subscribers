0e300ee62457f646b854723ae3a10e7bb60a34a0
mtutarinov <mihailtutarinov2@gmail.com>
2025-07-07 11:18:10 +0300

    add subscription repo

---
+++ app/repositories/subscription.py
@@ -0,0 +1,27 @@
+from uuid import UUID
+
+from app.database.models import Subscription
+from app.repositories.base import BaseRepository
+
+class SubscriptionRepository(BaseRepository):
+
+    async def create(self, user_id: UUID) -> bool:
+        subscription = Subscription(
+            _id=user_id,
+            active=True,
+        )
+        await self.collection.insert_one(subscription)
+        return True
+
+    async def update(self, data: dict, subscription_id: UUID):
+        await self.collection.update_one(
+            {'_id': subscription_id},
+            {'$set': data}
+        )
+        return True
+
+
+
+
+
+
